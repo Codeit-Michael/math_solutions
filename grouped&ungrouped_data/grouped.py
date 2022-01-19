3f0077ea72cd0c5a132a011a8981716025bff4db
Michael Maranan <michaelmaranan030@gmail.com>
2022-01-19 11:29:45 +0800

    add class interval

--- grouped&ungrouped_data/grouped.py
+++ grouped&ungrouped_data/grouped.py
@@ -1,4 +1,22 @@
+from prettytable import PrettyTable
+
 # for grouped data
+class ClassInterval():
+	def __init__(self,lowest:int,highest:int,class_size:int):
+		self.lowest = lowest
+		self.highest = highest+1
+		self.class_size = class_size
+		self.single_class = {}
+
+		for x in range(self.lowest,self.highest,self.class_size):
+			self.single_class[x] = []
+			for y in range(x,x+self.class_size):
+					self.single_class[x].append(y)
+
+	def class_interval(self):
+		return self.single_class
+
+
 class mean():
 	# total_fx = ∑𝑓𝑋
 	# N = 𝑁 
@@ -21,18 +39,21 @@ class mean():
 		return mean_answer
 
 
-if __name__ == '__main__':
-	f = [3,5,9,12,5,4,2]
-	x = [122,131,140,149,158,167,176]
-
-	d1 = mean(f,x)
-	print(d1.fx_finder())
-	print(d1.get_mean())
-
-
 class median():
+	# LT_cumf = < cum f
 	pass
 
 
 class mode():
 	pass
+
+
+if __name__ == '__main__':
+	ci = ClassInterval(118,180,9)
+	print(ci.class_interval())
+
+	f = [3,5,9,12,5,4,2]
+	x = [122,131,140,149,158,167,176]
+	d1 = mean(f,x)
+	print(d1.fx_finder())
+	print(d1.get_mean())
