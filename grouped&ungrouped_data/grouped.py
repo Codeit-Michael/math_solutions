ad8dea0f2fec5c2c0b052d2e9a876da31ed59fc8
Michael Maranan <michaelmaranan030@gmail.com>
2022-01-22 16:58:08 +0800

    add solution representation

--- grouped&ungrouped_data/grouped.py
+++ grouped&ungrouped_data/grouped.py
@@ -2,6 +2,7 @@ from prettytable import PrettyTable
 
 # for grouped data
 class ClassInterval():
+
 	def __init__(self,lowest:int,highest:int,class_size:int):
 		self.lowest = lowest
 		self.highest = highest+1
@@ -15,11 +16,13 @@ class ClassInterval():
 					self.single_class[self.numlist_move].append(y)
 			self.numlist_move += 1
 
+
 	def class_interval_table(self):
 		CI_table = PrettyTable(['class interval'])
 		for x in range(len(self.single_class)):
 			CI_table.add_row([f'{self.single_class[x][0]} - {self.single_class[x][-1]}'])
 		return CI_table
+
 
 
 class mean(ClassInterval):
@@ -31,18 +34,21 @@ class mean(ClassInterval):
 		self.x = x
 		self.fx = []
 
+
 	def _fx_finder(self):
 		for i in range(0,len(self.f)):
 			new = (self.f[i] * self.x[i])
 			self.fx.append(new)
 		return self.fx
 
+
 	def _mean_solution(self,Efx,n,total):
-		mytable = PrettyTable(['mean'])
-		mytable.add_row(['Efx/N\n'])
-		mytable.add_row([f'{Efx}\n---\n{n}\n'])
-		mytable.add_row([f'= {total}'])
-		return mytable
+		meantable = PrettyTable(['mean'])
+		meantable.add_row(['Efx/N\n'])
+		meantable.add_row([f'{Efx}\n---\n{n}\n'])
+		meantable.add_row([f'= {total}'])
+		return meantable
+
 
 	def get_mean(self):
 		self._fx_finder()
@@ -52,8 +58,10 @@ class mean(ClassInterval):
 		return self._mean_solution(total_fx,N,mean_answer)
 
 
+
 class median(ClassInterval):
 	# cumf = </> cum f
+	# N = N/2
 	# fmBefore = ∑𝑓𝑋-1
 
 	def __init__(self,f:list,cumf:list,lowest:int,highest:int,class_size:int):
@@ -63,7 +71,15 @@ class median(ClassInterval):
 
 
 	def _median_solution(self,N,fmBefore,Lm,fm,i,total_median):
-		pass
+		mediantable = PrettyTable(['median'])
+		mediantable.add_row(['((N/2) - Efx-1)\nLm + ---------------- i\nfm\n'])
+		mediantable.add_row([f'(({N*2}/2) - {fmBefore})\n{Lm} + --------------- {i}\n{fm}\n'])
+		mediantable.add_row([f'({N} - {fmBefore})\n{Lm} + --------------- {i}\n{fm}\n'])
+		mediantable.add_row([f'({N - fmBefore})\n{Lm} + --------------- {i}\n{fm}\n'])
+		mediantable.add_row([f'{Lm} + ({(N-fmBefore)/fm}) {i}\n'])
+		mediantable.add_row([f'{Lm} + {((N-fmBefore)/fm)*i}\n= {total_median}'])
+		return mediantable
+
 
 	def get_median(self):
 		N = self.cumf[-1]//2
@@ -81,19 +97,21 @@ class median(ClassInterval):
 		return self._median_solution(N,fmBefore,Lm,fm,i,total_median)
 
 
+
 class mode(ClassInterval):
 	pass
 
 
+
 if __name__ == '__main__':
+	ci = ClassInterval(118,180,9)
 	f = [3,5,9,12,5,4,2]
 	x = [122,131,140,149,158,167,176]
 	cumf = [3,8,17,29,34,38,40]
-	ci = ClassInterval(118,180,9)
 
 	my_mean = mean(f,x)
 	print(my_mean.get_mean())
 
 	my_median = median(f,cumf,118,180,9)
-	print(my_median.class_interval_table())
+	# print(my_median.class_interval_table())
 	print(my_median.get_median())
